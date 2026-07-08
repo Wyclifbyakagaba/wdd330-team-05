e4fbb764fe63546683b0c797d6427b217370dbd0
tsbowers <trevorbowers3@gmail.com>
2026-07-08 15:18:11 -0500

    dynamic product list

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,4 @@
+import { renderListWithTemplate } from "./utils.mjs";
 export default class ProductList {
     constructor(category, dataSource, listElement) {
         this.category = category;
@@ -9,4 +10,22 @@ export default class ProductList {
         const list = await this.dataSource.getData();
         this.renderList(list);
     }
+    renderList(list) {
+        // const htmlStrings = list.map(productCardTemplate);
+        // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+        renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", true);
+    }
+}
+
+function productCardTemplate(product) {
+    return `
+        <li class="product-card">
+        <a href="product_pages/?product=${product.Id}">
+            <img src="${product.Image}" alt="${product.Name}">
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">${product.Description}</h2>
+            <p class="product-card__price">$${product.FinalPrice}</p>
+        </a>
+        </li>
+    `;
 }
